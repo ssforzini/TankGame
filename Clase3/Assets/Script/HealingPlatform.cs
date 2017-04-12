c480f21189b12b0f89e8e79c56a56e51244080f0
ssforzini <sebasforzini@hotmail.com>
2017-04-11 21:34:49 -0300

    Commit Inicial con proyecto base en UNITY. 2 tanques, 1 escena

---
+++ Clase3/Assets/Script/HealingPlatform.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealingPlatform : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionStay(Collision col){
+		if(col.gameObject.name == "Tanque" || col.gameObject.name == "TanqueEnemigo"){
+			if(col.gameObject.GetComponent<TankMovement> ().life > 0){
+
+				if (col.gameObject.name == "Tanque") {
+					if(GameObject.Find ("TankLife").GetComponent<Slider> ().value < 1.0f){
+						GameObject.Find ("TankLife").GetComponent<Slider> ().value -= 0.001f;
+					}
+				} else {
+					if (GameObject.Find ("EnemyTankLife").GetComponent<Slider> ().value < 1.0f) {
+						GameObject.Find ("EnemyTankLife").GetComponent<Slider> ().value -= 0.001f;
+					}
+				}
+				if(col.gameObject.GetComponent<TankMovement> ().life < 50f){
+					col.gameObject.GetComponent<TankMovement> ().life += 0.05f;
+				}
+
+				print (col.gameObject.GetComponent<TankMovement> ().life);
+			}
+		}
+	}
+}
---
+++ Clase3/Assets/Script/TorretMovement.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class TorretMovement : MonoBehaviour {
+
+	public GameObject prefab;
+	public Transform puntoSalida;
+
+	public KeyCode leftMovement;
+	public KeyCode rightMovement;
+	public KeyCode shoot;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(leftMovement)){
+			transform.Rotate (Vector3.down * Time.deltaTime * 30f);
+		}
+
+		if(Input.GetKey(rightMovement)){
+			transform.Rotate (Vector3.up * Time.deltaTime * 30f);
+		}
+
+		if(Input.GetKeyDown(shoot)){
+			if (GetComponentInParent<TankMovement> ().alive) {
+				Instantiate (prefab, puntoSalida.position, puntoSalida.rotation);
+			}
+
+		}
+	}
+}
