df4d0f29d813dadee8df9b252f284ded451d4c81
ssforzini <sebasforzini@hotmail.com>
2017-05-11 15:16:47 -0300

    Modificaciones Varias
    Se realizaron modificaciones en el mapa:
    1) Nuevo terreno -> Falta terminar obviamente
    2) Se agrego un nuevo mesh
    3) Se modifico el mesh del puente, ahora tiene colliders
    
    Hay que eliminar todos los meshes que no sumen, igual que las texturas

---
+++ Assets/Script/Bala.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bala : MonoBehaviour {
+
+	public float fuerza;
+	// Use this for initialization
+	void Start () {
+		GetComponent<Rigidbody> ().AddRelativeForce (Vector3.right * fuerza, ForceMode.Impulse);
+		Destroy (gameObject,2.0f);
+	}
+
+	void OnCollisionEnter(Collision col){
+		if(col.gameObject.name == "Tanque" || col.gameObject.name == "TanqueEnemigo"){
+			if(col.gameObject.GetComponent<TankMovement> ().life > 0){
+
+				if (col.gameObject.name == "Tanque") {
+					GameObject.Find ("TankLife").GetComponent<Slider> ().value += 0.20f;
+				} else {
+					GameObject.Find ("EnemyTankLife").GetComponent<Slider>().value += 0.20f;
+				}
+
+				col.gameObject.GetComponent<TankMovement> ().life -= 10f;
+				Destroy (gameObject);
+			}
+		}
+	}
+}
