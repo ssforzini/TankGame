08718670f247d2278cd3fc55c82893a835a1df86
ssforzini <sebasforzini@hotmail.com>
2017-05-16 22:09:16 -0300

    Se commitean todas las coasas que faltaban commitear anteriormente y el mapa terminado Tambien cambios en el movimiento del personaje

--- Clase3/Assets/Script/TankMovement.cs
+++ Clase3/Assets/Script/TankMovement.cs
@@ -2,14 +2,18 @@
 
 public class TankMovement : MonoBehaviour {
 
-	public KeyCode upMovement;
-	public KeyCode downMovement;
-	public KeyCode rightMovement;
-	public KeyCode leftMovement;
 	public float life;
 	public bool alive;
+	[SerializeField] private float speed;
+	[SerializeField] private float rotateSpeed;
 
-	public KeyCode[] array;
+	public KeyCode[] keys;
+	public GameObject[] checkpoints;
+
+	private float velocityForward;
+	private float valocityBack;
+	private bool collisionTerrain;
+
 
 	// Use this for initialization
 	void Start () {
@@ -19,21 +23,33 @@ public class TankMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetKey(upMovement)){
-			transform.Translate (Vector3.right * Time.deltaTime * 10f);
+		//GetComponent<Rigidbody>().AddForce(-transform.up * Time.deltaTime * 7500f);
+
+		if(Input.GetKeyDown(keys[4])){
+			transform.position = checkpoints[Random.Range(0,checkpoints.Length)].GetComponent<Transform>().position;
+			transform.rotation = new Quaternion (0,0,0,0);
 		}
 
-		if(Input.GetKey(downMovement)){
-			transform.Translate (Vector3.left * Time.deltaTime * 10f);
+		if(collisionTerrain){
+			if(Input.GetKey(keys[0])){
+				//transform.Translate (Vector3.right * Time.deltaTime * speed);
+				GetComponent<Rigidbody>().AddForce(transform.right * Time.deltaTime * speed);
+			}
+
+			if(Input.GetKey(keys[1])){
+				//transform.Translate (Vector3.left * Time.deltaTime * speed);
+				GetComponent<Rigidbody>().AddForce(-transform.right * Time.deltaTime * speed);
+			}
+
+			if(Input.GetKey(keys[2])){
+				transform.Rotate (Vector3.up * Time.deltaTime * rotateSpeed);
+			}
+
+			if(Input.GetKey(keys[3])){
+				transform.Rotate (Vector3.down * Time.deltaTime * rotateSpeed);
+			}
 		}
 
-		if(Input.GetKey(rightMovement)){
-			transform.Rotate (Vector3.up * Time.deltaTime * 25f);
-		}
-
-		if(Input.GetKey(leftMovement)){
-			transform.Rotate (Vector3.down * Time.deltaTime * 25f);
-		}
 
 		if(life == 0){
 			foreach (Renderer r in GetComponentsInChildren<Renderer>()) {
@@ -44,6 +60,18 @@ public class TankMovement : MonoBehaviour {
 			alive = false;
 		} else if(life > 50f){
 			life = 50f;
+		}
+
+	}
+
+	void OnCollisionStay(Collision col){
+		if(col.gameObject.name == "Terrain" || col.gameObject.tag == "Bridges"){
+			collisionTerrain = true;
+		}
+	}
+	void OnCollisionExit(Collision col){
+		if(col.gameObject.name == "Terrain" || col.gameObject.tag == "Bridges"){
+			collisionTerrain = false;
 		}
 	}
 }
